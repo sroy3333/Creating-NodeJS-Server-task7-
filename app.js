99f612dee5dbc402ceb7aa0986c1bb7453278679
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-02-18 13:43:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     
     let responseText = '';
-    let statusCode = 200;
     if (req.url === '/home') {
         responseText = 'Welcome home';
     } else if (req.url === '/about') {
@@ -11,11 +11,10 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/node') {
         responseText = 'Welcome to my Node.js project';
     } else {
-        statusCode = 404;
         responseText = 'Page not found';
     }
 
-    res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+   res.setHeader('content-type', 'text/plain');
     res.write(responseText);
     res.end();
 });
